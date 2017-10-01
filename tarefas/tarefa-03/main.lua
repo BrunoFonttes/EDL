aa680070a27034ccb8140c703f57b8d9527a59a7
Ingryd <ingrydmoura27@hotmail.com>
2017-10-01 02:52:50 -0300

    tarefa-03 adicionada

---
+++ tarefas/tarefa-03/main.lua
@@ -0,0 +1,8 @@
+ 
+function love.draw()
+	love.graphics.print("Hello World", 400, 300)
+	-- Seta a cor no texto --
+	love.graphics.setColor(255,255,255)
+	-- Seta a cor do fundo --  
+	love.graphics.setBackgroundColor(578,10,0)  
+end
