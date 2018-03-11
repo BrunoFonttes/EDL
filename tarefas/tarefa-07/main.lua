801020050c41458dbea2e235a0ae3ab6aed5292d
BrunoBF <brunobfonttes@gmail.com>
2018-03-11 00:02:13 -0300

    Tarefa-07 adicionada(a completar)

--- tarefas/tarefa-07/main.lua
+++ tarefas/tarefa-07/main.lua
@@ -13,11 +13,7 @@ birds = {img =nil}
 newBirds = {}
 lastTimeBird = 1000
 
-
 gravity = 50
--- Tarefa-06
--- Variável:player
--- Tipo:Registro
 player = { x = 100, y = floorHeight+100,jumping = false, jumpCont=3000, accel = jumpAccel, img = nil }
 jumCont = 2
 gameOver = false;
@@ -41,9 +37,6 @@ function love.update(dt)
 			player.jumpCont= player.jumpCont+10
 			aux = 0
 		end
-		-- Tarefa-06
-		-- Variável:love.keyboard
-		-- Tipo:Enum
 		if love.keyboard.isDown('w') then
 
 			if player.y > 150 and player.jumpCont>100 then
@@ -86,77 +79,84 @@ function love.update(dt)
 			player.accel = jumpAccel
 			gravity = initGravity
 		end
-		--gera Obstaculos
-		lastTimeObstacle = lastTimeObstacle - 6
-		if lastTimeObstacle <= 0 then
-			lastTimeObstacle = love.math.random(200,winWidth)
-
-			newObstacle= { x = winWidth , y = floorHeight, width = 20, height = 50, counted = false}
-			-- Tarefa-06
-			-- Variável:obstacles
-			-- Tipo: Array
-			table.insert(obstacles, newObstacle)
-		end
-
-		--gera Passaros
-		lastTimeBird = lastTimeBird - 3
-		if lastTimeBird <= 0 then
-			lastTimeBird = love.math.random(200,winHeight)
-			newBird= { x = winWidth , y = 150 - love.math.random(0,50), width = 25, height = 25, counted = false}
-			table.insert(birds, newBird)	
-		end
 		
-		--remove Obstaculos
-		for i, obstacle in ipairs(obstacles) do
-			obstacle.x = obstacle.x - 10
 
-			if obstacle.x < 0 then
-				table.remove(obstacle, i)
+		--Tarefa-07
+		--Escopo: <<A incluir>>
+		--Tempo de Vida: Os obstáculos são gerados de modo parcialmente randômico (parâmetro lastTimeObstacle)
+		--removidos assim que deixam de estar vísiveis para o usuário,
+		--ou seja assim que a posição .x do obstáculo é inferior a zero.
+
+		--gera Obstaculos 
+		--Quando lastTimeObstacle atinge 0 gera um novo obstáculo e a lastTimeObstacle passa
+		--a assumir o novo valor gerado aleatoriamente entre 200 e a largura da tela.
+				lastTimeObstacle = lastTimeObstacle - 6
+				if lastTimeObstacle <= 0 then
+					lastTimeObstacle = love.math.random(200,winWidth)
+
+					newObstacle= { x = winWidth , y = floorHeight, width = 20, height = 50, counted = false}
+					table.insert(obstacles, newObstacle)
+				end
+				
+				--remove Obstaculos
+				for i, obstacle in ipairs(obstacles) do
+					obstacle.x = obstacle.x - 10
+
+					if obstacle.x < 0 then
+						table.remove(obstacle, i)
+					end
+
+					if obstacle.counted == false and obstacle.x < player.x then
+						obstacle.counted = true
+						counter = counter + 1
+					end
+				end
+
+
+				--gera Passaros
+				--Equivalente a geração de obstáculos.
+				lastTimeBird = lastTimeBird - 3
+				if lastTimeBird <= 0 then
+					lastTimeBird = love.math.random(200,winHeight)
+					newBird= { x = winWidth , y = 150 - love.math.random(0,50), width = 25, height = 25, counted = false}
+					table.insert(birds, newBird)	
+				end
+
+				--remove Passaro
+				--Equivalente a remoção de obstáculos.
+				for i, bird in ipairs(birds) do
+					bird.x = bird.x - 7
+
+					if bird.x < 0 then
+						table.remove(bird,i)
+					end
+				end
+
+				--Checa Colisao c obstaculo
+				for i, obstacle in ipairs(obstacles) do
+					if CheckCollision(player.x,player.y,player.img:getWidth(),player.img:getHeight(),obstacle.x,obstacle.y,obstacle.width,obstacle.height) then
+						gameOver = true
+					end
+				end
+
+				--Checa colisao c passaro
+				for i, bird in ipairs(birds) do
+					if CheckCollision(player.x,player.y,player.img:getWidth(),player.img:getHeight(),bird.x,bird.y,bird.width,bird.height) then
+						table.remove(bird,i)
+						bird.y=1000
+						player.jumpCont = player.jumpCont+300
+					end
+				end
 			end
-
-			if obstacle.counted == false and obstacle.x < player.x then
-				obstacle.counted = true
-				counter = counter + 1
-			end
-		end
-
-		--remove Passaro
-		for i, bird in ipairs(birds) do
-			bird.x = bird.x - 7
-
-			if bird.x < 0 then
-				table.remove(bird,i)
-			end
-		end
-
-		--Checa Colisao c obstaculo
-		for i, obstacle in ipairs(obstacles) do
-			if CheckCollision(player.x,player.y,player.img:getWidth(),player.img:getHeight(),obstacle.x,obstacle.y,obstacle.width,obstacle.height) then
-				gameOver = true
-			end
-		end
-
-		--Checa colisao c passaro
-		for i, bird in ipairs(birds) do
-			if CheckCollision(player.x,player.y,player.img:getWidth(),player.img:getHeight(),bird.x,bird.y,bird.width,bird.height) then
-				table.remove(bird,i)
-				bird.y=1000
-				player.jumpCont = player.jumpCont+300
-			end
-		end
-	end
+	--Fim do bloco de comentário da tarefa-07
 end
+ 
 
-
+ 
 function love.draw()
 	qtdGrad = 5
 	grad = 8
 	gradHeight = 50/qtdGrad
-	--Tarefa-06
-	--Variável: tipo
-	--Tipo:Tupla
-	tipo = {"fill","line"}
-
 	--desenha o chao
 	for y=0,qtdGrad do
 		love.graphics.setColor(71-y*grad, 85-y*grad, 96-y*grad)
@@ -173,25 +173,22 @@ function love.draw()
     love.graphics.setFont(font)
    	love.graphics.print(counter, 650, 10)
 
+   	
    	--desenha o jogador
    	love.graphics.print(player.jumpCont,50,10)
 
    	--desenha os obstaculos
     for i, obstacle in ipairs(obstacles) do
       love.graphics.setColor(40,47,56)
-	  love.graphics.rectangle('fill', obstacle.x, obstacle.y, obstacle.width, obstacle.height)
+	  love.graphics.rectangle('fill', obstacle.x, obstacle.y, obstacle.width, obstacle.height);
 	  love.graphics.setColor(32,39,47)
 	  love.graphics.rectangle('fill',obstacle.x-obstacle.width,obstacle.y-obstacle.height, 3* obstacle.width, obstacle.height)
 	end
 
 	--desenha os passaros
 	for i, bird in ipairs(birds) do
-		setTipo = tipo[1]
-		if(i%2 == 0) then
-			setTipo = tipo[2]
-		end
 	  love.graphics.setColor(196,196,196)
-	  love.graphics.rectangle(setTipo, bird.x, bird.y, bird.width, bird.height);
+	  love.graphics.rectangle('fill', bird.x, bird.y, bird.width, bird.height);
 	end
 
 	--desenha o gameover
